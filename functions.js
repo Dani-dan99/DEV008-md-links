952084911a0f3f0e6dde7621842ff233336a18dc
Dani-dan99 <ldcpdan@gmail.com>
2023-08-25 12:49:57 -0600

    convertToAbsolute

--- functions.js
+++ functions.js
@@ -4,14 +4,32 @@ const fs = require("fs");
 const chalk = require("chalk");
 
 const allPath =
-'C:\\Users\\ldcpd\\Desktop\\Md-Lindks\\DEV008-md-links\\examplesOfLinks\\linksMd.md';
+  "C:\\Users\\ldcpd\\Desktop\\Md-Lindks\\DEV008-md-links\\examplesOfLinks\\linksMd.md";
 
 const thePathExistOrNot = (allPath) => {
   if (fs.existsSync(allPath)) {
     return true;
   } else {
     return false;
-  };
+  }
 };
 console.log(chalk.bold.blue(thePathExistOrNot(allPath)));
 console.log(chalk.blue("Great! The path exist"));
+
+//se valida si la ruta es relativa o absoluta.
+//se resuelve la Ruta como absoluta (se convierte ruta relativa a absoluta)
+function convertToAbsolute(allPath) {
+  if (path.isAbsolute(allPath)) {
+    return allPath;
+  } else {
+    const absolutePath = path.join(process.cwd(), allPath);
+    return absolutePath;
+  }
+}
+console.log(chalk.bold.yellow(convertToAbsolute(allPath)));
+console.log(chalk.yellow("Is a absolute path"));
+
+module.exports = {
+  thePathExistOrNot,
+  convertToAbsolute,
+};
